f78ca5a5b1fc1c064ea690273f5e4f24826b3664
ibolonkin <ivanbolonkin111@gmail.com>
2026-02-16 17:50:21 +0500

    Создана начальная архитектура фронтенда

---
+++ frontend/src/components/OrderCard/OrderCard.jsx
@@ -0,0 +1,16 @@
+const OrderCard = () => {
+    return (
+        <div className="card-container">
+            <div className="card-left-status">
+            </div>
+            <div className="card-first-row">
+                <h3 className="order-num">{/*{orderNum}*/}</h3>
+                <p className="card-date"></p>
+                <p className="card-project-name"></p>
+            </div>
+            
+        </div>
+    );
+}
+ 
+export default OrderCard;
